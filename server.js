778b8b0b17b0dc76185430a0424848b0b3b864ec
Akun unduhan <akun.unduhan21@gmail.com>
2019-08-18 21:07:42 +0700

    merubah server.js untuk dialogflow webhook

--- server.js
+++ server.js
@@ -1,20 +1,46 @@
+//express here
 const express = require('express');
 const app = express();
+
+//path joining
 const path = require('path');
+const dfkeyPath = path.join(__dirname,'dfkey.json');
+
+//dotenv config
 const dotenv = require('dotenv');
 dotenv.config();
 
+//body parser
 const bodyParser = require('body-parser');
 
+/*dialogflow
+const { SessionClient } = require('dialogflow');
+const dialogflowClient = new SessionClient({
+    keyFileName:dfkeyPath
+});
+*/
+
+//twilio
 const client = require('twilio')(process.env.TWILLIO_ACCOUNT_ID,process.env.TWILLIO_AUTH_ID);
 const MessagingService = require('twilio').twiml.MessagingResponse;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
 
-//https://timberwolf-mastiff-9776.twil.io/demo-reply
+/**
+ * start routing here
+ * yeeyyeye
+ */
 app.get('/',(req,res)=>{
-    res.send('yeay im here');
+    res.send('yeay im v1.0');
+});
+
+app.post('/webhook',(req,res)=>{
+    const data = req.body;
+    const twiml = new MessagingService();
+    twiml.message('yeay!! this is from hook');
+    res.writeHead(200,{'content-type':'text/xml'});
+    res.end(twiml.toString());
 });
 
 app.post('/incoming',(req,res)=>{
