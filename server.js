7257c0d493aa8dc014bece2a8cc08970677555da
Akun unduhan <akun.unduhan21@gmail.com>
2019-08-18 21:28:35 +0700

    merubah server.js menjadi sederhana

--- server.js
+++ server.js
@@ -1,67 +1,15 @@
-//express here
 const express = require('express');
-const app = express();
-
-//path joining
-const path = require('path');
-const dfkeyPath = path.join(__dirname,'dfkey.json');
-
-//dotenv config
-const dotenv = require('dotenv');
-dotenv.config();
-
-//body parser
 const bodyParser = require('body-parser');
 
-/*dialogflow
-const { SessionClient } = require('dialogflow');
-const dialogflowClient = new SessionClient({
-    keyFileName:dfkeyPath
-});
-*/
-
-//twilio
-const client = require('twilio')(process.env.TWILLIO_ACCOUNT_ID,process.env.TWILLIO_AUTH_ID);
-const MessagingService = require('twilio').twiml.MessagingResponse;
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:false}));
+const app = express();
 app.use(bodyParser.json());
-
-/**
- * start routing here
- * yeeyyeye
- */
-app.get('/',(req,res)=>{
-    res.send('yeay im v1.0');
-});
 
 app.post('/webhook',(req,res)=>{
     const data = req.body;
-    console.log(data);
-    const twiml = new MessagingService();
-    twiml.message('yeay!! this is from hook');
-    res.writeHead(200,{'content-type':'text/xml'});
-    res.end(twiml.toString());
-});
 
-app.post('/incoming',(req,res)=>{
-  console.log(req.body);
-  const msgContent = req.body.Body;
-  const twiml = new MessagingService();
+    const response = {
+        fulfillmentText: "your webhook work fine";
+    }
 
-  if(msgContent === '/itsme'){
-    twiml.message("hi dimas, selamat datang di bot pertama kamu");
-  }else if(msgContent === '/itsdimaswife'){
-    twiml.message("Hai fitri, salam ya dari aku, Bot buatan suamimu, dimas editiya. Semoga sehat selalu,love you so much");
-  }else{
-    twiml.message("percakapan belum ada, kedepannya pasti lebih baik");
-  }
-
-  res.writeHead(200,{'content-type':'text/xml'});
-  res.end(twiml.toString());
-});
-
-app.listen(process.env.PORT,(req,res)=>{
-    console.log(`listening on port ${process.env.PORT}`);
+    res.json(response);
 });
